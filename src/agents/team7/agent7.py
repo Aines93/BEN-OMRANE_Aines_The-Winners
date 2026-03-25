8129719848bd84315b9b3a45f10ed8c635d45759
Aines_93 <ainesbenomrane06@gmail.com>
2026-03-25 09:45:33 +0100

    agent7 qui avance sur le circuit pendant 200 pas de temps avec mon nom affiché

--- src/agents/team7/agent7.py
+++ src/agents/team7/agent7.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from .steering import Steering
 
 from utils.track_utils import compute_curvature, compute_slope
 from agents.kart_agent import KartAgent
@@ -11,23 +12,41 @@ class Agent7(KartAgent):
         self.agent_positions = []
         self.obs = None
         self.isEnd = False
-        self.name = "Team7" # replace with your chosen name
+        self.name = "BEN-OMRANE Aines" 
+        self.steering=Steering()
 
     def reset(self):
         self.obs, _ = self.env.reset()
         self.agent_positions = []
+        self.steering.reset()
 
     def endOfTrack(self):
         return self.isEnd
 
     def choose_action(self, obs):
-        acceleration = random.random()
-        steering = random.random()
+        points = obs.get("paths_start",[]) # On récupère la liste des points
+        
+        if len(points) <= 2: # Si la longueur de la liste est inferieur à 2, on accèlère à fond (ligne d'arrivée proche)
+            return {
+                "acceleration": 1.0,
+                "steer": 0.0,
+                "brake": False,
+                "drift": False,
+                "nitro": True,
+                "rescue":False,
+                "fire": False,
+            }
+        
+        target = points[2] # On récupère le x-ème point de la liste defini par la variable de classe
+        gx = target[0] # On récupère x, le décalage latéral
+        gz = target[2] # On récupère z, la profondeur
+
+        steering =self.steering.manage_pure_pursuit(gx,gz,2)
         action = {
-            "acceleration": acceleration,
+            "acceleration": 1,
             "steer": steering,
             "brake": False, # bool(random.getrandbits(1)),
-            "drift": bool(random.getrandbits(1)),
+            "drift": False,
             "nitro": bool(random.getrandbits(1)),
             "rescue":bool(random.getrandbits(1)),
             "fire": bool(random.getrandbits(1)),
---
+++ src/agents/team7/steering.py
@@ -0,0 +1,42 @@
+import math
+import numpy as np
+
+class Steering:
+    """
+    Module Steering : Gère la logique de direction
+    """
+    
+    def __init__(self) -> None:
+        """Initialise les variables d'instances de l'agent."""
+        
+        
+        pass
+
+    def reset(self) -> None:
+        """Réinitialise les variables d'instances de l'agent expert"""
+        pass
+        
+    def manage_pure_pursuit(self,gx:float,gz:float,gain:float) -> float:
+        """
+        Gère la logique de direction grâce au pure pursuit
+
+        Args:
+            
+            gx(float) : Décalage latéral de la cible.
+            gz(float) : Profondeur de la cible.
+            gain(float) : Gain à appliquer à l'angle final.
+        
+        Returns:
+            
+            float : Variable donnant la direction des roues.
+        """
+        
+        l2 = gx**2 + gz**2 # calcul de l'hypoténuse
+            
+        if l2 < 0.01 : return 0.0 # Si on est déjà sur la cible, on ne tourne pass
+            
+        angle = math.atan2(2 * 2 * gx,l2) # Calcul de la formule issu de pure_pursuit et du modèle bicyclette
+
+        steer = angle * gain # application du coefficient
+        
+        return np.clip(steer,-1,1) # Ajout d'une sécurité pour garder le steer entre -1 et 1
